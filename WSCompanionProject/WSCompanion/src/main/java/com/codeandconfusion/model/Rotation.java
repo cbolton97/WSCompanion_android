035344ab1845efc34c11d05874fec23df053d8ff
Chris Bolton <chris.bolton97@gmail.com>
2014-01-08 15:40:48 -0800

    Initial draft of rotation object

---
+++ WSCompanionProject/WSCompanion/src/main/java/com/codeandconfusion/model/Rotation.java
@@ -0,0 +1,19 @@
+package com.codeandconfusion.model;
+
+public class Rotation {
+    private String date;
+    private String structure;
+    private String order;
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getStructure() {
+        return structure;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+}
